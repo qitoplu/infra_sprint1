b7992ac3168365c42be3523eab161a9aef32adda
root <root@epd42mjqu2kplek9ulb4.auto.internal>
2023-06-19 14:09:04 +0000

    done

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,17 @@
 import os
+from os import getenv
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+load_dotenv(find_dotenv())
 
-DEBUG = True
+SECRET_KEY = getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['158.160.12.141', '127.0.0.1', 'localhost', 'infrasprintdomain.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +94,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -110,3 +117,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
